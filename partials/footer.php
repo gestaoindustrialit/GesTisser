b2bcf268ca3509b3384679de388861a3de89dc3d
Ricardo Pereira <gestaoindustrial.it@calcadacorp.ch>
2026-07-23 11:43:19 +0100

    Refactor footer.php for authenticated shell handling

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,12 @@
-</main>
+<?php if (!empty($gtHasAuthenticatedShell)): ?>
+        </main>
+    </div>
+</div>
+<?php else: ?>
+    </main>
+<?php endif; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="assets/app.js"></script>
+<script src="assets/mapper-layout.js"></script>
 </body>
 </html>
