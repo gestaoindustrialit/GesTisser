7f496b9bcb3b18a1a3ee4191a42b3c896bcef6f6
Ricardo Pereira <gestaoindustrial.it@calcadacorp.ch>
2026-09-08 15:39:30 +0100

    Tornar importação de clientes mais flexível

--- app/Services/CustomerSpreadsheet.php
+++ app/Services/CustomerSpreadsheet.php
@@ -7,6 +7,8 @@ final class CustomerSpreadsheet
     {
         return [
             'codigo'=>'code', 'nome'=>'name', 'nif'=>'tax_number', 'pais'=>'country',
+            'codigo_cliente'=>'code', 'cod_cliente'=>'code', 'cliente'=>'name',
+            'nome_cliente'=>'name', 'nome_fiscal'=>'name', 'razao_social'=>'name',
             'prefixo_pais'=>'country_prefix', 'telefone'=>'phone', 'telemovel'=>'mobile',
             'email'=>'email', 'morada_1'=>'address', 'morada_2'=>'address_2', 'cidade'=>'city',
             'codigo_postal'=>'postal_code', 'fax'=>'fax', 'contacto'=>'contact_name',
